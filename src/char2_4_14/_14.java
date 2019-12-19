f36e54a124d723b253daa0d2854fc8ae04a9eda8
Season000 <43396528+Season000@users.noreply.github.com>
2019-12-19 17:12:28 +0800

    Initial commit

---
+++ src/char2_4_14/_14.java
@@ -0,0 +1,26 @@
+package char2_4_14;
+
+interface A{
+    public static final String MSG = "hello";//全局常量
+    public abstract void print();//抽象方法
+}
+interface B{
+    public abstract void get();
+}
+class X implements A,B{
+    public void print(){
+        System.out.println("hello print");
+    }
+    public void get(){
+        System.out.println("hello get");
+    }
+}
+
+
+
+public class _14 {
+    public static void main(String args[]){
+        X x = new X();
+        x.print();
+    }
+}
