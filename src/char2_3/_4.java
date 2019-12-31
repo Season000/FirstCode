83b30cc5fb615b6aa737f80d9c959faaee2dcfbc
Season000 <43396528+Season000@users.noreply.github.com>
2019-12-31 13:49:05 +0800

    12.32

---
+++ src/char2_3/_4.java
@@ -0,0 +1,13 @@
+package char2_3;
+//数组
+public class _4 {
+    public static void main(String args[]){
+        int data [] = new int [3];
+        data[0] = 1;
+        data[1] = 2;
+        data[2] = 3;
+        for(int i = 0 ; i < data.length ; i ++){
+            System.out.println(data[i]);
+        }
+    }
+}
